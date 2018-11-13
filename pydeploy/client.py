80f411ce40a8822fe32b085f8195090a6db36d54
Tony Han <itony9401@live.com>
2018-11-13 03:43:34 -0800

    added spacing

--- pydeploy/client.py
+++ pydeploy/client.py
@@ -16,6 +16,7 @@ def print_objects(s3, bucket):
     try:
         for obj in s3.Bucket(bucket).objects.all():
             print(obj.key)
+
     except ClientError as err:
         print('Unable to list bucket: {0}. '.format(bucket) + str(err))
 
